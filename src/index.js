f39bfcd286f8c07aedd19a62c51a8cc491e35801
Afimia Fidelis <38649892+Izebiafe@users.noreply.github.com>
2023-08-18 20:50:50 +0000

    Added an interactive list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './styles/style.css';
 import Todo from './modules/data.js';
 import UI from './modules/ui.js';
+import handleStatusUpdate from './modules/statusUpdates.js';
 
 const ui = new UI();
 const form = document.querySelector('.form');
 const clearBtn = document.querySelector('.clearBtn');
-
 document.addEventListener('DOMContentLoaded', UI.displayFromLocalStorage);
-
 // Add todo list
 form.addEventListener('submit', (e) => {
   const desc = document.querySelector('.desc').value;
@@ -16,29 +15,23 @@ form.addEventListener('submit', (e) => {
   if (newId.length > 0) {
     index = newId[newId.length - 1].index + 1;
   } else {
-    index = 0;
+    index = 1;
   }
 
   const completed = false;
-
   if (desc === '') {
     ui.errorMsg('Error', 'rgba(255, 0, 0, 0.5)');
   } else {
     // Init todo data
     const newTodo = new Todo(index, desc, completed);
-
     // Display data on ui
     ui.displayTask(newTodo);
-
     UI.addToLocalStorage(newTodo);
-
     ui.errorMsg('Success', 'rgba(9, 186, 9, 0.5)');
-
     document.querySelector('.desc').value = '';
   }
-
   e.preventDefault();
 });
-
 // Clear all task
 clearBtn.addEventListener('click', UI.clearCompletedTasks);
+handleStatusUpdate();
